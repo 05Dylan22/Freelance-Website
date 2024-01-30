4723e33b2a034efd2075234dd7c6b7ffc411a909
Dylan Anderson <anderson.dylan.522@gmail.com>
2024-01-30 15:36:20 -0500

    hero nav basics and base styling

--- src/PageElements/HomePageNav.tsx
+++ src/PageElements/HomePageNav.tsx
@@ -1,11 +1,13 @@
+import "../styles/HomeHeroNav.css"
 import { Link } from "react-router-dom"
 
 const HomePageNav = () => {
   return (
-    <header>
+    <header className="b-flex home-header">
       <p>FREE-EZ</p>
-      <nav>
-        <Link to={"/"}></Link>
+      <nav className="a-flex home-header-nav">
+        <Link className="link-no-style nav-link" to={"/"}>Log In</Link>
+        <Link className="link-no-style nav-link"  to={"/"}>Find Work</Link>
       </nav>
     </header>
   )
