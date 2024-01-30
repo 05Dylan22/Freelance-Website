a3b4905a40fd9374caa3faecac137964655a0cf1
Dylan Anderson <anderson.dylan.522@gmail.com>
2024-01-30 16:23:25 -0500

    two nav buttons and more base styling

--- src/PageElements/HomePageNav.tsx
+++ src/PageElements/HomePageNav.tsx
@@ -6,8 +6,8 @@ const HomePageNav = () => {
     <header className="b-flex home-header">
       <p>FREE-EZ</p>
       <nav className="a-flex home-header-nav">
-        <Link className="link-no-style nav-link" to={"/"}>Log In</Link>
-        <Link className="link-no-style nav-link"  to={"/"}>Find Work</Link>
+        <Link className="link-no-style nav-link btm-brdr" to={"/"}>Log In</Link>
+        <Link className="link-no-style nav-link brdr-shdow"  to={"/"}>Find Work</Link>
       </nav>
     </header>
   )
