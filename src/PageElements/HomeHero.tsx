f45b83598f31b86ed799414cf32b930592584677
Dylan Anderson <anderson.dylan.522@gmail.com>
2024-01-31 11:30:55 -0500

    hero large finished, needs responsive styling

--- src/PageElements/HomeHero.tsx
+++ src/PageElements/HomeHero.tsx
@@ -3,16 +3,19 @@ import { Link } from "react-router-dom"
 
 const HomeHero = () => {
   return (
-    <section className="home-hero-section">
-      <h1 className="main-title home-hero-title">Start Finding Freelance Work Today</h1>
-      <p>With only 3 simple steps, you can start filling your client pipeline</p>
-      <div className="b-flex home-hero-ctas">
-        <Link className="link-no-style brdr-shdow hero-btn" to={"/"}>Find Work</Link>
-        <p>OR</p>
-        <Link className="link-no-style solid-btn hero-btn" to={"/"}>Find Freelancers</Link>
-      </div>
+    <>
+      <section className="home-hero-section">
+        <img className="hero-img" src="https://media.istockphoto.com/vectors/freelance-developer-or-designer-working-at-homevector-character-vector-id1069451598?k=6&m=1069451598&s=612x612&w=0&h=zKaxANFPp9Snmx59rn9pbc5nn4oKlD7_-bO-wc8gpsc=" alt="Woman working on laptop" />
+        <h1 className="main-title home-hero-title">Start Finding Freelance Work Today</h1>
+        <p>With only 3 simple steps, you can start filling your client pipeline</p>
+        <div className="b-flex home-hero-ctas">
+          <Link className="link-no-style brdr-shdow hero-btn" to={"/"}>Find Work</Link>
+          <p>OR</p>
+          <Link className="link-no-style solid-btn hero-btn" to={"/"}>Find Freelancers</Link>
+        </div>
+      </section>
       <h2 className="title find-work-title">More Ways To Find Work</h2>
-    </section>
+    </>
   )
 }
 
