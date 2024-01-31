4512c90060d8ef4758f59d30ec6520d8fb6e5e87
Dylan Anderson <anderson.dylan.522@gmail.com>
2024-01-30 22:25:18 -0500

    responsive design of home hero nav minus permanant logo

--- src/PageElements/HomePageNav.tsx
+++ src/PageElements/HomePageNav.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 const HomePageNav = () => {
   return (
     <header className="b-flex home-header">
-      <p>FREE-EZ</p>
+      <p className="temp-logo">FREE-EZ</p>
       <nav className="a-flex home-header-nav">
         <Link className="link-no-style nav-link btm-brdr" to={"/"}>Log In</Link>
         <Link className="link-no-style nav-link brdr-shdow"  to={"/"}>Find Work</Link>
