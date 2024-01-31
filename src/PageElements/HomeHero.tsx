5f24d6556c30a2284f6631d2ca79d16055eeb2d9
Dylan Anderson <anderson.dylan.522@gmail.com>
2024-01-30 23:52:51 -0500

    home hero titles and buttons

--- src/PageElements/HomeHero.tsx
+++ src/PageElements/HomeHero.tsx
@@ -1,8 +1,17 @@
+import "../styles/HomeHero.css"
+import { Link } from "react-router-dom"
+
 const HomeHero = () => {
   return (
-    <section>
-      <h1 className="main-title">Start Finding Freelance Work Today</h1>
-      <p className="text-center">Start using our perfect client aquisition trifecta to fill your pipeline and let your stress melt away as finding work becomes as easy as 1, 2, 3</p>
+    <section className="home-hero-section">
+      <h1 className="main-title home-hero-title">Start Finding Freelance Work Today</h1>
+      <p>With only 3 simple steps, you can start filling your client pipeline</p>
+      <div className="b-flex home-hero-ctas">
+        <Link className="link-no-style brdr-shdow hero-btn" to={"/"}>Find Work</Link>
+        <p>OR</p>
+        <Link className="link-no-style solid-btn hero-btn" to={"/"}>Find Freelancers</Link>
+      </div>
+      <h2 className="title find-work-title">More Ways To Find Work</h2>
     </section>
   )
 }
