6f9c9bcc8b8001521ed1de028b7f02bbb0a4dd69
Dylan Anderson <anderson.dylan.522@gmail.com>
2024-02-04 23:05:29 -0500

    useObserver handles an array of HTMLElements

--- src/Hooks/useObserver.tsx
+++ src/Hooks/useObserver.tsx
@@ -1,12 +1,16 @@
-const useObserver = (element: HTMLElement) => {
+const useObserver = (elements: Array<HTMLElement>) => {
   const observer = new IntersectionObserver((entry) => {
     entry.forEach(entry => {
       if (!entry.isIntersecting) return
-      element.style.animation = "slideIn ease-in 0.2s forwards"
-      observer.unobserve(element)
+      const observedEl = entry.target as HTMLElement
+      observedEl.style.animation = "ease-in slideIn 0.25s forwards"
+      observer.unobserve(entry.target)
     })
-  }, {threshold: 0.15})
-  observer.observe(element)
+  }, {threshold: 0.25})
+
+  elements.forEach(element => {
+    observer.observe(element)
+  })
 }
 
 export default useObserver
--- src/PageElements/Ways2FindWork.tsx
+++ src/PageElements/Ways2FindWork.tsx
@@ -1,17 +1,31 @@
-import { MutableRefObject, useEffect, useRef } from "react"
+import { useEffect, useRef } from "react"
 import "../styles/Ways2FindWork.css"
 import useObserver from "../Hooks/useObserver"
 
 const Ways2FindWork = () => {
-  const element = useRef<HTMLElement | null>(null)
+  const createAndShareEl = useRef<HTMLDivElement | null>(null)
+  const browseEl = useRef<HTMLDivElement | null>(null)
+  const beFoundEl = useRef<HTMLDivElement | null>(null)
+
   useEffect(() => {
-    if (!element.current) return
-    useObserver(element.current)
+    if (!createAndShareEl.current || !browseEl.current || !beFoundEl.current) return
+    useObserver([createAndShareEl.current, browseEl.current, beFoundEl.current])
   })
 
   return (
-    <section ref={element} className="temp-styling">
-      Next section on home page
+    <section className="ways-to-find-container">
+      <div ref={createAndShareEl} className="way-to-find-work">
+        <p>Create And Share</p>
+        <p>Create your website when you sign up and use the link to share with your social networks and even run ads to it.</p>
+      </div>
+      <div ref={browseEl} className="way-to-find-work">
+        <p>Browse Listings</p>
+        <p>Scroll job posting on our website and apply to all the ones you are a good fit for.</p>
+      </div>
+      <div ref={beFoundEl} className="way-to-find-work">
+        <p>Found In Search</p>
+        <p>Be active on your profile everyday to help boost your chances of being found in search results and invited onto the job.</p>
+      </div>
     </section>
   )
 }
